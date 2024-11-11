36ad822e285ba3b544c4fb4bfd18cb9d719d175f
Ben Condemi <bcondemi@andrew.cmu.edu>
2024-11-11 13:24:01 -0500

    addressing tab/spacing issues

--- src/translate/index.js
+++ src/translate/index.js
@@ -3,7 +3,7 @@
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
-    // Edit the translator URL below
+    // Edit the translator URL below\
     const TRANSLATOR_API = 'https://d-buggers-translate-hmfmgsg4cpa5fcfp.canadacentral-01.azurewebsites.net';
     const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
     const data = await response.json();
