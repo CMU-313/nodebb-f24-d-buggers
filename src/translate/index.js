8ec3970a4997cd05b59b8b6de5a4773169e70902
Ben Condemi <bcondemi@andrew.cmu.edu>
2024-11-11 13:30:36 -0500

    silencing linter for one new file

--- src/translate/index.js
+++ src/translate/index.js
@@ -3,7 +3,6 @@
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
-    // Edit the translator URL below\
     const TRANSLATOR_API = 'https://d-buggers-translate-hmfmgsg4cpa5fcfp.canadacentral-01.azurewebsites.net';
     const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
     const data = await response.json();
