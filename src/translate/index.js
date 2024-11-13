f607ad0fc730b7431899f0545c1bfb5ffdfc5ae3
bencondemi <105668786+bencondemi@users.noreply.github.com>
2024-11-12 21:19:02 -0500

    Update index.js

--- src/translate/index.js
+++ src/translate/index.js
@@ -3,7 +3,7 @@
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
-    const TRANSLATOR_API = 'https://translate-d-buggers.azurewebsites.net';
+    const TRANSLATOR_API = 'https://translate-d-buggers.azurewebsites.net/';
     const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
     const data = await response.json();
     return [data.is_english, data.translated_content];
